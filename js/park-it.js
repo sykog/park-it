bbfdf34aaca3b48076befd5a9d475ba1082edf39
Dracologist <ejckanzler@yahoo.com>
2018-05-03 14:46:29 -0400

    Attempted to do the map init

--- js/park-it.js
+++ js/park-it.js
@@ -1,4 +1,9 @@
 // waits till page is loaded
 $(document).ready(function() {
-    alert("javascript loaded");
+    function initMap() {
+        var mapDiv = new google.maps.Map(document.getElementById("map"), {
+            center: {lat: 47.313582, lng: -122.1800072},
+            zoom: 6
+        });
+    }
 });
