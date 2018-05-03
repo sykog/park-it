e56a4463f68f5f43e4755036824e82fd0f1b6981
Antonio Suarez <asuarez2@mail.greenriver.edu>
2018-05-03 14:50:31 -0400

    got the map to load

--- js/park-it.js
+++ js/park-it.js
@@ -1,9 +1,6 @@
-// waits till page is loaded
-$(document).ready(function() {
-    function initMap() {
-        var mapDiv = new google.maps.Map(document.getElementById("map"), {
-            center: {lat: 47.313582, lng: -122.1800072},
-            zoom: 6
-        });
-    }
-});
+function initMap() {
+    var mapDiv = new google.maps.Map(document.getElementById("map"), {
+        center: {lat: 47.313582, lng: -122.1800072},
+        zoom: 6
+    });
+}
