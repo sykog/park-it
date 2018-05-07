2f8f4fa66feb208ad6d7f49d0e271af13d646d29
Dracologist <ejckanzler@yahoo.com>
2018-05-07 15:05:22 -0400

    Added button functionality

--- js/park-it.js
+++ js/park-it.js
@@ -4,3 +4,16 @@ function initMap() {
         zoom: 6
     });
 }
+
+$("#park").click(function(){
+    alert("Set parking location");
+});
+
+
+$("#retrieve").click(function(){
+    alert("Get parking location");
+});
+
+$("#gotIt").click(function(){
+    $("#instructions").hide();
+});
